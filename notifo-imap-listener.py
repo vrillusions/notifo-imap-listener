87eccf75f0b2ba36f969c966f1408661ce1bf539
Todd Eddy <vr@vrillusions.com>
2010-06-23 23:16:45 -0400

    Use proper logging

--- notifo-imap-listener.py
+++ notifo-imap-listener.py
@@ -16,6 +16,7 @@ import urllib
 import urllib2
 import json
 import time
+import logging
 from base64 import encodestring
 from ConfigParser import ConfigParser
 
@@ -24,25 +25,27 @@ class ImapMonitor():
     """The class that monitors the mail account"""
     
     def __init__(self, user, password, server='localhost', ssl=True):
+        self.logger = logging.getLogger('ImapMonitor')
         self.server = server
         self.ssl = ssl
         self.user = user
         self.password = password
         self.notifo = Notifo()
         # if you get errors here, make sure server supports SSLv2
-        print 'Connecting...'
+        self.logger.info('Connecting...')
         self.mail = imaplib.IMAP4_SSL(host=server)
         #self.mail.debug = 4
-        print 'Logging in...'
+        self.logger.info('Logging in...')
         self.mail.login(user, password)
-        print 'Success'
+        self.logger.debug('Success')
 
     def _handle_error(self, error=None):
-        print 'ImapMonitor() error: %s' % error
+        self.logger.critical(error)
         self.cleanup()
         sys.exit(1)
         
     def cleanup(self):
+        self.logger.debug('Logging out')
         self.mail.logout()
     
     def run_once(self):
@@ -52,6 +55,7 @@ class ImapMonitor():
         if typ != 'OK':
             self._handle_error('Did not receive ok from server (search): %s' % typ)
         for num in msgnums[0].split():
+            self.logger.debug('Received new email')
             #typ, header = self.mail.fetch(num, '(BODY.PEEK[HEADER])')
             #if typ != 'OK':
             #    self._handle_error('Did not receive ok from server (fetch header): %s' % typ)
@@ -72,6 +76,7 @@ class ImapMonitor():
             else:
                 text = msg.get_payload()
             text = text.strip()
+            self.logger.debug('Text sent to notifo: %s' % text)
             # TODO: if something fails the message won't retry
             self.notifo.send_notification(msg=text)
             #print '-----'
@@ -79,7 +84,7 @@ class ImapMonitor():
             #print '-----'
         
     def run_forever(self):
-        print 'Running forever, use ctrl-c to cancel'
+        self.logger.info('Running forever, use ctrl-c to cancel')
         while True:
             self.run_once()
             # Wait for one second so we don't hammer the mail server
@@ -89,6 +94,7 @@ class ImapMonitor():
 class Notifo():
     """Basic notifo interface (didn't bother with subscription request)."""
     def __init__(self):
+        self.logger = logging.getLogger('Notifo')
         self.base_url = 'https://api.notifo.com/v1/'
         config = ConfigParser()
         config.read('config.ini')
@@ -103,7 +109,7 @@ class Notifo():
         if msg is not None:
             data['msg'] = msg
         else:
-            print 'no message specified, aborting.'
+            self.logger.info('no message specified, aborting.')
             return False
         if label is not None:
             data['label'] = label
@@ -122,20 +128,22 @@ class Notifo():
             response = urllib2.urlopen(request)
         except urllib2.HTTPError, e:
             # may have an error for why it doesn't work
-            print 'Error: %s' % e.fp.readline()
+            self.logger.error(e.fp.readline())
             return None
         except urllib2.URLError, e:
             # a non http error, may be a typo
-            print 'Error: %s' % e.reason
+            self.logger.error(e.reason)
             return None
         try:
             result = json.loads(response.read())
         except ValueError, e:
-            print 'Error parsing response: %s' % e
+            self.logger.error('Error parsing response: %s' % e)
+        
         if result['status'] == 'success':
+            self.logger.debug('Message sent successfuly')
             return True
         else:
-            print 'Error: %s' % result['status']
+            self.logger.error(result['status'])
             return None
     
             
@@ -150,19 +158,41 @@ def main():
     imap_user = config.get('mail', 'user')
     imap_password = config.get('mail', 'password')
     
+    # configure logging
+    LEVELS = {'debug': logging.DEBUG,
+              'info': logging.INFO,
+              'warning': logging.WARNING,
+              'error': logging.ERROR,
+              'critical': logging.CRITICAL}
+    level = LEVELS.get(config.get('logging', 'level'), logging.NOTSET)
+    logging.basicConfig(level=level,
+                        format="%(asctime)s: %(name)s: %(levelname)s: %(message)s",
+                        filename=config.get('logging', 'file'),
+                        filemode='a')
+    consolelog = logging.StreamHandler()
+    consolelog.setLevel(level)
+    consolelog.setFormatter(logging.Formatter("%(levelname)-8s: %(message)s"))
+    logging.getLogger('').addHandler(consolelog)
+    logmain = logging.getLogger('main')
+    
     monitor = ImapMonitor(server=imap_server, ssl=imap_ssl, user=imap_user, 
                           password=imap_password)
     while True:
         try:
             monitor.run_forever()
-            monitor.cleanup()
         except imaplib.IMAP4.abort, e:
             # server error, need to start over
-            print "Server error: %s " % e
-            print "reconnecting in 30 seconds"
+            logmain.warning("Server error: %s " % e)
+            logmain.info("reconnecting in 30 seconds")
             time.sleep(30)
             monitor.__init__(server=imap_server, ssl=imap_ssl, user=imap_user, 
                              password=imap_password)
+        except KeyboardInterrupt, e:
+            # Ctrl-c
+            logmain.debug('Received ctrl-c or SIGINT, cleaning up')
+            monitor.cleanup()
+            # not raising since everything has been handled
+            sys.exit(0)
     
 
 if __name__ == "__main__":
